d2d4be69e0edd65455be88bbef3509181c4842bf
zhouhuiMr <1430433546@qq.com>
2017-12-14 17:00:00 +0800

    游戏的简单实现

--- faerie/js/faerie/faerie.js
+++ faerie/js/faerie/faerie.js
@@ -3,23 +3,38 @@
         this.game = game;
         this.body = null;
 
+        //object attribute
+        this.HEALTH = 5;
+        this.ATK = 1;
+        this.DEF = 0;
+
         // object init
         this.init();
     }
     faerie.prototype = {
         init : function(){
-            console.info(this.game)
-            this.body = this.game.add.sprite(90,99,'faerie');
+            this.body = this.game.add.sprite(120,this.game.height/2,'faerie');
+            this.body.anchor.set(0.5,0.5);
+            this.body.smoothed = true;
+            this.game.physics.enable(this.body,Phaser.Physics.ARCADE);
+            this.body.body.setSize(40,70,40,30);
+            this.body.body.collideWorldBounds = true;
             //faerie action of fly
             this.body.animations.add("fly",["faerie_fly1.png","faerie_fly2.png","faerie_fly3.png"]);
-            this.body.animations.add("attack",["faerie_attack1.png","faerie_attack2.png","faerie_attack3.png"]);
+            this.body.animations.add("attack",["faerie_attack1.png","faerie_attack2.png","faerie_attack3.png","faerie_attack4.png"]);
             this.fly();
         },
         fly : function(){
             this.body.animations.play("fly",10,true);
         },
         attack : function(){
-            this.body.animations.play("attack",7,false);
+            var atk = this.body.animations.play("attack",10,false);
+            var f = this;
+            atk.onComplete.add(function(sprite, animation){
+                if(sprite.frame == 3){
+                    f.fly();
+                }
+            },this)
         }
     }
     window.faerie = faerie;
--- faerie/js/main.js
+++ faerie/js/main.js
@@ -1,6 +1,10 @@
 var GAMEWIDTH = 960,
     GAMEHEIGHT = 540;
 
+var STARTSCENE = null,
+    GAMESCENE = null,
+    ENDSCENE = null;
+
 var game = new Phaser.Game(GAMEWIDTH, GAMEHEIGHT, Phaser.AUTO, 'gamecontainer', {
     preload: preload,
     create: create,
@@ -23,32 +27,59 @@ function preload(){
     game.load.atlas('faerie','/faerie/resource/faerie/faerie.png','/faerie/resource/faerie/faerie.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 }
 
+var player = null,
+    keycontroller = null;
 
 // init the object of game
 function create(){
     //set world bounds
-    game.world.setBounds(0, 0, GAMEWIDTH, GAMEWIDTH);
+    game.world.setBounds(0, 0, GAMEWIDTH, GAMEHEIGHT);
 
     //set physics engine
     game.physics.startSystem(Phaser.ARCADE);
 
     //set back ground
-    game.stage.backgroundColor = "#324a62";
+    game.stage.backgroundColor = "#77b8f8";
 
     //set scale mode
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-    new mainScene(game);
+
+
+    GAMESCENE = new mainScene(game);
+
+    player = GAMESCENE.Player;
+
+    keycontroller = GAMESCENE.KeyController;
+
 }
 
 //refresh frame of game
 function update(){
+    player.body.body.velocity.x = 0;
+    player.body.body.velocity.y = 0;
 
+    if(keycontroller.UP.isDown){
+        player.body.body.velocity.y = -200;
+    }
+    if(keycontroller.DOWN.isDown){
+        player.body.body.velocity.y = 200;
+    }
+    if(keycontroller.LEFT.isDown){
+        player.body.body.velocity.x = -200;
+    }
+    if(keycontroller.RIGHT.isDown){
+        player.body.body.velocity.x = 200;
+    }
+    if(keycontroller.ATK.isDown){
+        player.attack();
+    }
 }
 
 //debug game
 function render(){
-
+    game.debug.body(player.body);
+    game.debug.spriteInfo(player.body, 32, 32);
 }
 
 /*================================*/
@@ -92,12 +123,25 @@ function loadcomplete(){
     //main scene of game
     var mainScene = function(game){
         this.game = game;
+        this.KeyController = null;
+
+        this.Player = null;
+
         this.init();
     }
     mainScene.prototype = {
         init : function(){
+            //init key board controller
+           this.KeyController = this.game.input.keyboard.addKeys({
+                "UP":Phaser.Keyboard.W,
+                "DOWN":Phaser.Keyboard.S,
+                "LEFT":Phaser.Keyboard.A,
+                "RIGHT":Phaser.Keyboard.D,
+                "ATK":Phaser.Keyboard.J,
+            });
             //create faerie
-            var player = new faerie(this.game);
+            this.Player = new faerie(this.game);
+
         }
     }
     window.mainScene = mainScene;
